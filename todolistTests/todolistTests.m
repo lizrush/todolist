b39d4ef02b60c3fcf27b21b9d5e1ebe0cc4694c1
liz rush <liz@lizmrush.com>
2014-08-02 14:15:40 -0700

    Initial Commit

---
+++ todolistTests/todolistTests.m
@@ -0,0 +1,34 @@
+//
+//  todolistTests.m
+//  todolistTests
+//
+//  Created by liz on 2/8/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface todolistTests : XCTestCase
+
+@end
+
+@implementation todolistTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
